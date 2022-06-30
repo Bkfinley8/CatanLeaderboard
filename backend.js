00ccae64de59c72baf7f4526885c794cc45cf4ee
Maxfojtik <maxfojtik@bex.net>
2022-06-30 11:38:56 -0400

    added most of networking

--- backend.js
+++ backend.js
@@ -1,5 +1,5 @@
 
-// var wsUri = "ws://localhost:12389"; // Localhost
+// var wsUri = "ws://localhost:8080"; // Localhost
 var wsUri = "ws://192.168.1.47:8080"; // Max's
 
 class BackendConnection {
@@ -15,6 +15,9 @@ class BackendConnection {
 
 	onOpen(evt) {
 		console.log("CONNECTED");
+		this.startGame([{"name":"Maxwell","color":"Blue"},{"name":"Chloe","color":"White"},{"name":"Donovan","color":"Red"},{"name":"Cory","color":"Orange"},{"name":"Johnathan","color":"Brown"},{"name":"Zach","color":"Green"}]);
+		setTimeout(this.addSettlement, 2000, "Chloe");
+		setTimeout(this.addCity, 5000, "Chloe");
 	}
 
 	onClose(evt) {
@@ -24,11 +27,30 @@ class BackendConnection {
 
 	onMessage(evt) {
 		console.log('<-: ' + evt.data);
-		var params = evt.data.split("|");
-		if(params[0]=="gotoAdminWait")
+		// var params = evt.data.split("|");
+		var message = JSON.parse(evt.data);
+		if(message.request=="gotoAdminWait")
 		{
-			gotoStartPage();
+			gotoAdminStartPage();
 		}
+		if(message.request=="gotoAdminPlayer")
+		{
+			gotoAdminPlayPage();
+		}
+		if(message.request=="gotoPlayerWait")
+		{
+			gotoPlayerWait();
+		}
+		if(message.request=="gotoPlayerPlay")
+		{
+			gotoPlayerPlay();
+		}
+		if(message.request=="fill")
+		{
+			json in message.data
+			// gotoPlayerPlay();
+		}
+		
 	}
 
 	onError(evt) {
@@ -40,13 +62,74 @@ class BackendConnection {
 		console.log("->: " + message);
 	}
 
-	startGame(playerList)
+	addSettlement(name)
+	{
+        var message = {"request": "addSettlement", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+	addCity(name)
+	{
+        var message = {"request": "addCity", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+
+	removeSettlement(name)
+	{
+        var message = {"request": "removeSettlement", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+	removeCity(name)
+	{
+        var message = {"request": "removeCity", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+
+	claimKnight(name)
+	{
+        var message = {"request": "claimKnight", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+	claimRoad(name)
+	{
+        var message = {"request": "claimRoad", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+
+	addVP(name)
+	{
+        var message = {"request": "addVP", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+	removeVP(name)
+	{
+        var message = {"request": "removeVP", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+
+	
+	addDevCard(name)
+	{
+        var message = {"request": "addDevCard", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+	removeDevCard(name)
+	{
+        var message = {"request": "removeDevCard", "name": name};
+        connection.send(JSON.stringify(message));
+	}
+
+	startGame(pl)
     {
-        var message = {"request": "startGame", "playerList": playerList};
-        this.send(JSON.stringify(message));
+        var message = {"request": "startGame", "playerList": pl};
+        connection.send(JSON.stringify(message));
     }
 
-    adminClicked(){
-        this.send(JSON.stringify({"request": "adminClicked"}));
+    adminClicked()
+	{
+        connection.send(JSON.stringify({"request": "adminClicked"}));
     }
+	playerClicked() 
+	{
+		connection.send(JSON.stringify({"request": "playerClicked"}));
+	}
 }
